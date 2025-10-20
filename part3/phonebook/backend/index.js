00c98dcd6a4ffbf1aae3e74a897959bc27a9837d
Theodore Abshire <TheodoreAbshire@Gmail.com>
2025-10-19 22:49:52 -0700

    Handle validation errors.

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -103,6 +103,8 @@ app.use((error, request, response, next) => {
   console.log('Error:', error.message)
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 })
