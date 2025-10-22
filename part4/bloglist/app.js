b4d2d57ed316ec4baed73159b4d73b838728dcc8
Theodore Abshire <TheodoreAbshire@Gmail.com>
2025-10-22 03:00:54 -0700

    Broke code into sub-classes.

---
+++ part4/bloglist/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.connect(config.MONGODB_URI)
+
+const app = express()
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
