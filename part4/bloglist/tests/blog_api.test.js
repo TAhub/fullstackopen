abac4bf6779eae4187420d2cee49adefdd7c0556
Theodore Abshire <TheodoreAbshire@Gmail.com>
2025-10-27 01:37:03 -0700

    Added a test for POSTing.

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -16,7 +16,7 @@ describe('blogs', () => {
 
   test('returns the expected blogs', async () => {
     const blogs = (await api.get('/api/blogs')).body
-    assert.strictEqual(blogs.length, 1)
+    assert.strictEqual(blogs.length > 0, true, 'no blogs present')
     assert.strictEqual(blogs[0].title, 'On Instinct')
   })
 
@@ -24,6 +24,24 @@ describe('blogs', () => {
     const blogs = (await api.get('/api/blogs')).body
     assert.ok(blogs[0].id, '"id" is not present')
     assert.strictEqual(!blogs[0]._id, true, '"_id" is still present')
+  })
+
+  test('can post new blogs', async () => {
+    const oldBlogs = (await api.get('/api/blogs')).body
+    const newBlog = {
+      title: "Fake Blog",
+      author: "Author",
+      url: "https://fullstackopen.com/",
+      likes: Math.floor(Math.random(1000))
+    }
+    const result = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    const newBlogs = (await api.get('/api/blogs')).body
+    assert.strictEqual(newBlogs.length, oldBlogs.length + 1, 'blog count did not increase correctly')
+    assert.strictEqual(newBlogs[newBlogs.length - 1].likes, newBlog.likes, 'new blog was saved incorrectly')
   })
 })
 
