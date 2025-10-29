5318f5f8a8119d19ed2b93312b3ac1f5980413fa
Theodore Abshire <TheodoreAbshire@Gmail.com>
2025-10-29 02:39:29 -0700

    Added login token creation.

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -5,5 +5,6 @@ const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 const PASSWORD_HASH_SALT_ROUNDS = 10
+const TOKEN_SECRET = process.env.TOKEN_SECRET
 
-module.exports = { MONGODB_URI, PORT, PASSWORD_HASH_SALT_ROUNDS }
+module.exports = { MONGODB_URI, PORT, PASSWORD_HASH_SALT_ROUNDS, TOKEN_SECRET }
