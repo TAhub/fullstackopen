1921f059d0050d4e54c18b12888f7a71133f4ace
Theodore Abshire <TheodoreAbshire@Gmail.com>
2025-11-10 23:34:40 -0800

    Added a test for adding a blog.

--- part5/bloglist-e2e/tests/login.spec.js
+++ part5/bloglist-e2e/tests/login.spec.js
@@ -41,8 +41,38 @@ describe('Blog app', () => {
       await textboxes[1].fill('incorrect password dummy')
       await page.getByText('Login').click()
       // Afterwards, it should still be in the login form.
+      await expect(page.getByText('Failed to log in!')).toBeVisible()
       await expect(page.getByText('Login')).toBeVisible()
       await expect(page.getByText('Log Out')).not.toBeVisible()
+    })
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      const textboxes = await page.getByRole('textbox').all()
+      await textboxes[0].fill(testUsername)
+      await textboxes[1].fill(testPassword)
+      await page.getByText('Login').click()
+      // TODO: in a real test suite, I feel like this should be conditional on "succeeds with correct credentials"
+    })
+    
+    test('there are no blogs at first', async ({ page }) => {
+      await expect(page.getByText('View')).not.toBeVisible()
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await page.getByText('Create New Blog').click()
+      const textboxes = await page.getByRole('textbox').all()
+      await textboxes[0].fill('NEWTITLE')
+      await textboxes[1].fill('NEWAUTHOR')
+      await textboxes[2].fill('NEWURL')
+      await page.getByText('Create Blog').click()
+      // Give it some time.
+      await page.getByText('View').waitFor()
+      // There should now be a blog.
+      await expect(page.getByText('NEWTITLE')).toBeVisible()
+      await expect(page.getByText('NEWAUTHOR')).toBeVisible()
+      await expect(page.getByText('View')).toBeVisible()
     })
   })
 })
