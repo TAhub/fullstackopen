8fd8d02b4b170634087717349640f8b3227a2276
Theodore Abshire <TheodoreAbshire@Gmail.com>
2025-11-11 20:48:09 -0800

    Added more e2e tests.

--- part5/bloglist-e2e/tests/login.spec.js
+++ part5/bloglist-e2e/tests/login.spec.js
@@ -1,12 +1,19 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
+const baseUrl = 'http://localhost:3003/api'
 const testUsername = 'TESTUSER'
 const testPassword = 'TESTPASSWORD'
+
+const logInHelper = async (page, userName, password) => {
+  const textboxes = await page.getByRole('textbox').all()
+  await textboxes[0].fill(userName)
+  await textboxes[1].fill(password)
+  await page.getByRole('button', { name: 'Login' }).click()
+}
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
     // Reset database.
-    const baseUrl = 'http://localhost:3003/api'
     await request.post(`${baseUrl}/testing/reset`)
     await request.post(`${baseUrl}/users`, {
       data: {
@@ -49,13 +56,16 @@ describe('Blog app', () => {
 
   describe('When logged in', () => {
     beforeEach(async ({ page }) => {
-      const textboxes = await page.getByRole('textbox').all()
-      await textboxes[0].fill(testUsername)
-      await textboxes[1].fill(testPassword)
-      await page.getByRole('button', { name: 'Login' }).click()
+      await logInHelper(page, testUsername, testPassword)
       // TODO: in a real test suite, I feel like this should be conditional on "succeeds with correct credentials"
     })
-    
+
+    test('you can log out', async ({ page }) => {
+      await expect(page.getByRole('button', { name: 'Log Out' })).not.toBeVisible()
+      await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
+      await expect(page.getByRole('button', { name: 'Log Out' })).not.toBeVisible()
+    })
+
     test('there are no blogs at first', async ({ page }) => {
       await expect(page.getByRole('button', { name: 'View' })).not.toBeVisible()
     })
@@ -98,8 +108,32 @@ describe('Blog app', () => {
       test('it can be deleted', async ({ page }) => {
         page.on('dialog', dialog => dialog.accept())
         await page.getByRole('button', { name: 'Delete' }).click()
+        // TODO: this "wait for detached" seems very flakey...
+        // it's suppoedly "bad practice" but I think a static wait time would be better in this case
         await page.getByText('NEWTITLE').waitFor({ state: 'detached' })
         await expect(page.getByText('NEWTITLE')).not.toBeVisible()
+      })
+
+      test('it cannot be deleted if you are not the owner', async ({ page, request }) => {
+        // Register a second user to the DB.
+        const testUsername2 = testUsername + '2'
+        const testPassword2 = testPassword + '2'
+        await request.post(`${baseUrl}/users`, {
+          data: {
+            name: 'Name2',
+            userName: testUsername2,
+            password: testPassword2
+          }
+        })
+        // Log out, and login to the second user.
+        await page.getByRole('button', { name: 'Log Out' }).click()
+        await logInHelper(page, testUsername2, testPassword2)
+        // Open the blog.
+        await page.getByRole('button', { name: 'View' }).waitFor()
+        await page.getByRole('button', { name: 'View' }).click()
+        // It should open ,but there should not be a delete button.
+        await expect(page.getByText('likes 0')).toBeVisible()
+        await expect(page.getByRole('button', { name: 'Delete' })).not.toBeVisible()
       })
     })
   })
