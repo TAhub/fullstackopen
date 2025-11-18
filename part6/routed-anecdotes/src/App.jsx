38622e9ccc01c373bf0ba156dc67a573be0b2e58
Theodore Abshire <TheodoreAbshire@Gmail.com>
2025-11-18 01:30:46 -0800

    Add a router.

--- part6/routed-anecdotes/src/App.jsx
+++ part6/routed-anecdotes/src/App.jsx
@@ -1,3 +1,7 @@
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
 import { useState } from 'react'
 
 const Menu = () => {
@@ -6,9 +10,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link to='/' style={padding}>anecdotes</Link>
+      <Link to='/new' style={padding}>create new</Link>
+      <Link to='/about' style={padding}>about</Link>
     </div>
   )
 }
@@ -123,14 +127,16 @@ const App = () => {
   }
 
   return (
-    <div>
+    <Router>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/new" element={<CreateNew addNew={addNew} />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   )
 }
 
