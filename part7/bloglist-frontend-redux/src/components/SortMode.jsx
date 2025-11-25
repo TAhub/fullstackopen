c56612a22898d6fd53c908d6b6f82abf5fb555ee
Theodore Abshire <TheodoreAbshire@Gmail.com>
2025-11-24 20:09:27 -0800

    Fixed error with sort mode display.

--- part7/bloglist-frontend-redux/src/components/SortMode.jsx
+++ part7/bloglist-frontend-redux/src/components/SortMode.jsx
@@ -1,7 +1,7 @@
 const SortMode = ({ sortMode, setSortMode, sortModes }) => (
   <form>
     {sortModes.map(sm =>
-      <div>
+      <div key={sm}>
         <input type="radio" onChange={() => setSortMode(sm)} id={sm} name="sortMode" value={sm} checked={sm === sortMode} />
         <label for={sm}>{sm}</label>
       </div>
