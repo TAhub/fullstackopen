5cb0430e82969623da1e22ae9f52ff270a80ae20
Theodore Abshire <TheodoreAbshire@Gmail.com>
2025-11-26 00:10:13 -0800

    Broke store into separate file.

--- part7/bloglist-frontend-redux/src/main.jsx
+++ part7/bloglist-frontend-redux/src/main.jsx
@@ -4,17 +4,7 @@ import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 
 import App from './App'
-import notificationReducer from './reducers/notificationReducer'
-import blogReducer from './reducers/blogReducer'
-import userReducer from './reducers/userReducer'
-
-const store = configureStore({
-  reducer: {
-    notification: notificationReducer,
-    blogs: blogReducer,
-    user: userReducer
-  }
-})
+import store from './Store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
---
+++ part7/bloglist-frontend-redux/src/store.js
@@ -0,0 +1,13 @@
+import notificationReducer from './reducers/notificationReducer'
+import blogReducer from './reducers/blogReducer'
+import userReducer from './reducers/userReducer'
+
+const store = configureStore({
+  reducer: {
+    notification: notificationReducer,
+    blogs: blogReducer,
+    user: userReducer
+  }
+})
+
+export default store
