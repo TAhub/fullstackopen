b87f4d835f517dd02c6288f1880622a69533d3d0
Theodore Abshire <TheodoreAbshire@Gmail.com>
2025-11-27 01:42:46 -0800

    Give author filtering to allBooks

--- part8/library-backend-graphql/index.js
+++ part8/library-backend-graphql/index.js
@@ -107,7 +107,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book]!
+    allBooks(author: String): [Book]!
     allAuthors: [Author]!
   }
 `
@@ -116,7 +116,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (obj, args) => {
+      if (!args.author) {
+        return books
+      }
+      return books.filter(book => book.author === args.author)
+    },
     allAuthors: () => authors
   },
   Author: {
