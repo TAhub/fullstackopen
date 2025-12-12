412dfd5a500dd19d20b8992dc7aed054b94d47f1
Theodore Abshire <TheodoreAbshire@Gmail.com>
2025-12-11 19:41:07 -0800

    Configure the server to allow for subscriptions.

--- part8/library-backend-graphql/index.js
+++ part8/library-backend-graphql/index.js
@@ -1,6 +1,12 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const jwt = require('jsonwebtoken')
+const { expressMiddleware } = require('@as-integrations/express5')
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+const express = require('express')
+const cors = require('cors')
+const http = require('http')
 
 // Connect to database with Mongoose.
 const mongoose = require('mongoose')
@@ -20,20 +26,35 @@ const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
 
 // Start Apollo server.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
-startStandaloneServer(server, {
-  listen: { port: process.env.PORT },
-  context: async ({ req, res }) => {
-    const auth = req ? req.headers.authorization : null
-    if (auth && auth.startsWith('Bearer ')) {
-      const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
-      const currentUser = await User.findById(decodedToken.id)
-      return { currentUser }
-    }
-  },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+const startServer = async () => {
+  const app = express()
+  const httpServer = http.createServer(app)
+
+  const server = new ApolloServer({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+  })
+
+  await server.start()
+
+  app.use(
+    '/',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req, res }) => {
+        const auth = req ? req.headers.authorization : null
+        if (auth && auth.startsWith('Bearer ')) {
+          const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+          const currentUser = await User.findById(decodedToken.id)
+          return { currentUser }
+        }
+      },
+    })
+  )
+
+  httpServer.listen(process.env.PORT, () => {
+    console.log(`Server ready at http://localhost:${process.env.PORT}`)
+  })
+}
+startServer()
